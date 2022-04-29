2b1bfb98230842f2376a7e67d67a034774c6d11f
Alexandra Panas <alexandra.panas@why.de>
2022-04-29 18:49:04 +0200

    feat: add plugins

---
+++ plugins/scroll-lock.client.js
@@ -0,0 +1,23 @@
+export default (ctx, inject) => {
+  const $body = document.querySelector('body')
+  let scrollPosition = 0
+
+  const scrollLock = {
+    enable() {
+      scrollPosition = window.pageYOffset
+      $body.style.overflow = 'hidden'
+      $body.style.position = 'fixed'
+      $body.style.top = `-${scrollPosition}px`
+      $body.style.width = '100%'
+    },
+    disable() {
+      $body.style.removeProperty('overflow')
+      $body.style.removeProperty('position')
+      $body.style.removeProperty('top')
+      $body.style.removeProperty('width')
+      window.scrollTo(0, scrollPosition)
+    }
+  }
+
+  inject('scrollLock', scrollLock)
+}
