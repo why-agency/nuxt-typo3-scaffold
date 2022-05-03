e15ae6a7ad019350aceacfe6fc26cd1b477cdfd8
Alexandra Panas <alexandra.panas@why.de>
2022-05-03 10:06:53 +0200

    fix: add transparent bg to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,8 @@ module.exports = {
       white: '#fff',
       focus: '#6c9cde',
       error: '#d00f26',
-      success: '#89d27d'
+      success: '#89d27d',
+      transparent: 'transparent'
     },
     fontFamily: {
       primary: 'Favorit Std',
