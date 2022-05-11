bfabef231524dfbeb803f8e2d4897bf1c1796531
Alexandra Panas <alexandra.panas@why.de>
2022-05-11 10:09:21 +0200

    refactor: use base picture

--- components/OGridCardsIntro.vue
+++ components/OGridCardsIntro.vue
@@ -2,8 +2,7 @@
   <section class="relative py-10 lg:py-0 lg:pt-24 xl:pt-32">
     <!-- START background image -->
     <div
-      v-if="backgroundImage"
-      role="presentation"
+      v-if="image"
       class="
         absolute
         top-0
@@ -16,37 +15,7 @@
       "
       :class="{ dark: theme === 'dark' }"
     >
-      <picture v-if="backgroundImage">
-        <source
-          :srcset="`${backgroundImage.mobileSrc}?tr=w-375,h-600 1x, ${backgroundImage.mobileSrc}?tr=w-750,h-1200 2x, ${backgroundImage.mobileSrc}?tr=w-1125,h-1800 3x`"
-          media="(max-width: 375px)"
-        />
-        <source
-          :srcset="`${backgroundImage.mobileSrc}?tr=w-428,h-600 1x, ${backgroundImage.mobileSrc}?tr=w-856,h-1200 2x, ${backgroundImage.mobileSrc}?tr=w-1284,h-1800 3x`"
-          media="(max-width: 428px)"
-        />
-        <source
-          :srcset="`${backgroundImage.mobileSrc}?tr=w-768,h-600 1x, ${backgroundImage.mobileSrc}?tr=w-1536,h-1200 2x, ${backgroundImage.mobileSrc}?tr=w-2304,h-1800 3x`"
-          media="(max-width: 768px)"
-        />
-        <source
-          :srcset="`${backgroundImage.mobileSrc}?tr=w-1024,h-600 1x, ${backgroundImage.mobileSrc}?tr=w-2048,h-1200 2x`"
-          media="(max-width: 1023px)"
-        />
-        <source
-          :srcset="`${backgroundImage.src}?tr=w-1024,h-864 1024w, ${backgroundImage.src}?tr=w-1280,h-864 1280w, ${backgroundImage.src}?tr=w-1536,h-864 1536w, ${backgroundImage.src}?tr=w-1920,h-864 1920w, ${backgroundImage.src}?tr=w-2600,h-864 2600w`"
-          media="(min-width: 1024px)"
-        />
-        <img
-          loading="lazy"
-          :alt="backgroundImage.alt"
-          width="1920"
-          height="864"
-          :src="`${backgroundImage.src}?tr=w-1920,h-864`"
-          class="absolute top-0 left-0 object-cover w-full h-full"
-        />
-      </picture>
-
+      <BasePicture :images="image" size="h-full w-full" />
       <div
         class="absolute top-0 left-0 w-full h-full"
         :class="[
@@ -60,7 +29,7 @@
         ]"
       />
     </div>
-    <!-- END backround image -->
+    <!-- END background image -->
 
     <div v-if="formattedCards" class="relative">
       <!-- START grid header -->
@@ -190,17 +159,6 @@ export default {
         id: card.content?.id
         // TODO: headline layout prop should be removed from the API
       }))
-    },
-    backgroundImage() {
-      if (!this.image || !this.image.length) {
-        return
-      }
-
-      const image = this.image.flat()
-      const src = image[0].cdn.publicUrl
-      const mobileSrc = image[1]?.cdn.publicUrl || src
-
-      return { src, mobileSrc, alt: image[0]?.properties.alternative }
     },
     $_containerMarginTop() {
       return this.header.text || this.subline.text || this.actions
